a67f96fa0da2a41a00d6689d93fa3ed635c23281
gauravpawar47 <gauravpawar883@gmail.com>
2024-09-21 19:10:53 +0530

    ADD CH2

---
+++ 2. Operators/PracticeSet.java
@@ -0,0 +1,25 @@
+public class PracticeSet
+{
+    public static void main(String[] args)
+    {
+        // Q 1. Output
+
+        // --> 5, 4
+
+        // Q 2. Output
+
+        // --> Java
+
+        // Q 3. Output
+
+        // --> 4 0 2 
+
+        // Q 4. Output
+
+        // --> 278
+
+        // Q 5. Output
+
+        // --> 20, 20   
+    }
+}
